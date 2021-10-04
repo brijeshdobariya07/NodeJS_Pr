4d8493b4335a3c9257bad08d57073a41a5585862
Brijesh <48529265+brijeshdobariya07@users.noreply.github.com>
2021-10-04 11:01:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// const fs = require('fs');
+// fs.copyFileSync("file1.txt","file2.txt");
+
+
+var superheroes = require("superheroes");
+var superVillains = require("supervillains");
+
+var mySuperHeroes =  superheroes.random();
+var mySuperVillains = superVillains.random();
+
+console.log(mySuperHeroes);
+console.log(mySuperVillains);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
